6d4a84a1f610030ffe5add66901407929fb95f8a
moska <19352571+moskadev@users.noreply.github.com>
2022-12-14 11:03:19 +0100

    Projet créé

---
+++ Boardgames/BoardgamesApp.swift
@@ -0,0 +1,18 @@
+//
+//  BoardgamesApp.swift
+//  Boardgames
+//
+//  Created by Maxime MOSKALYK on 14/12/2022.
+//
+//
+
+import SwiftUI
+
+@main
+struct BoardgamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
