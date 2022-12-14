8aea1d96ec8751de9abdae21551d19da7ec02b87
moska <19352571+moskadev@users.noreply.github.com>
2022-12-14 16:23:36 +0100

    Restructuration des vues et ajout des images des jeux

--- Boardgames/View/GameListView.swift
+++ Boardgames/View/GameListView.swift
@@ -13,30 +13,71 @@ struct GameListView: View {
     var body: some View {
         ForEach(games) { game in
             NavigationLink(
-                    destination: {
-                        ScrollView {
-                            VStack {
-                                Text(game.name).font(.largeTitle)
-
-                                VStack(alignment: .leading) {
-                                    Text("Description :").font(.title2)
-                                    Text(game.description.trimmingCharacters(in: .whitespacesAndNewlines)).font(.title3)
-                                }
-                            }.padding()
-
-                        }
-                    },
-                    label: {
-                        HStack() {
-                        
-                            VStack(alignment: .center) {
-                                HStack {
-                                    Text(game.name).font(.title3)
-                                    Spacer()
-                                }
-                            }
-                    }
+                destination: {
+                    GameDetailView(game: game)
+                },
+                label: {
+                    GameCell(game: game)
                 })
+        }
+    }
+}
+
+
+struct GameImage: View {
+    var gameUrl: URL?
+    var body: some View {
+        AsyncImage(url: gameUrl) { phase in
+            switch phase {
+            case .empty:
+                ZStack {
+                    Color.clear
+                    ProgressView()
+                }
+            case .success(let image):
+                image
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+            default:
+                EmptyView()
+            }
+        }
+    }
+}
+
+struct GameCell: View {
+    var game: Game
+    var body: some View {
+        HStack() {
+            if game.thumbnailUrl != "" {
+                GameImage(gameUrl: game.thumbnail).frame(width: 130)
+            }
+
+        }
+        VStack(alignment: .center) {
+            HStack {
+                Text(game.name).font(.title3)
+                Spacer()
+            }
+        }
+    }
+}
+
+struct GameDetailView: View {
+    var game: Game
+
+    var body: some View {
+        ScrollView {
+            VStack {
+                GameImage(gameUrl: game.thumbnail)
+                Text(game.name).font(.largeTitle)
+
+                VStack(alignment: .leading) {
+                    Text("Description :").font(.title2)
+                    Text(game.description.trimmingCharacters(in: .whitespacesAndNewlines)).font(.title3)
+                }
+            }.padding()
+
         }
     }
 }
